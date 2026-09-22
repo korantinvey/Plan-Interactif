04fbb5eca0a95b84b76baea789f56295ed63ebfa
korantinvey <korantin.vey@gmail.com>
2026-09-22 13:00:59 +0200

    Fusionne « Le métro porte sa ligne en anneau, non en aplat » (#234)
    La signalétique du métro ne pose pas la couleur de la ligne en aplat : elle la donne au M dans son anneau et au rond du numéro, sur champ clair. Le mode gagne un drapeau « anneau », comme le tram avait « plaque », et l'indice se range dans une pastille pleine de la couleur de la ligne.

--- web/sw.js
+++ web/sw.js
@@ -60,7 +60,7 @@
  * qui était rangé sous la version d'avant : ces fonds et ces vignettes sont
  * dans le cache versionné, et c'est lui qu'on jette. C'est la dernière.
  */
-const VERSION = "6d9a9e86fa51";
+const VERSION = "91fc4988590c";
 const CACHE = "plan-" + VERSION;
 const DURABLE = "plan-durable";
 const HORS_LIGNE = "hors-ligne.html";
